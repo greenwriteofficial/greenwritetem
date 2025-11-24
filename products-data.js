952cc36bc7555a773ee6a93073da685fa87a1d5b
GreenWrite <greenwriteofficial@gmail.com>
2025-11-24 21:14:33 +0530

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -31,17 +31,6 @@ window.PRODUCTS = [
 ];
 
 
-// products-data.js
-import { db, collection, getDocs } from "./firebase.js";
 
-export async function getAllProducts() {
-  const products = [];
-  const snapshot = await getDocs(collection(db, "products"));
 
-  snapshot.forEach(doc => {
-    products.push({ id: doc.id, ...doc.data() });
-  });
-
-  return products;
-}
 
